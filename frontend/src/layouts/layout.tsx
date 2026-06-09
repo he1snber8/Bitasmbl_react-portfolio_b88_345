811d9fc0d10627a1a6a4b04e811cc07335b0678c
lukakhaja <lukakhaja@yahoo.com>
2026-06-09 17:09:20 +0400

    bitasmbl add: Define portfolio layout

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,6 +1,6 @@
 export default function layout() {
   return (
-    <div className="p-4 bg-green">
+    <div>
       <h1>Hello there</h1>
       <h1>Hello there, twice</h1>
       <h1>Reach me babyyy</h1>
